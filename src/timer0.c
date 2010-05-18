7d98903eb10c688698c8c5e3297ca4d64adef3f5
Manuel Huber <manuel.h87@gmail.com>
2010-05-18 12:07:28 +0200

    new timer0 module started

---
+++ src/timer0.c
@@ -0,0 +1,11 @@
+#include "timer0.h"
+
+unsigned char timer0_init_clock(unsigned int timing)
+{
+  
+}
+
+void timer0_shutdown(void)
+{
+  
+}
---
+++ src/timer0.h
@@ -0,0 +1,8 @@
+#ifndef _TIMER0_H_
+#define _TIMER0_H_
+
+unsigned char timer0_init_clock(unsigned int timing);
+
+void timer0_shutdown(void);
+
+#endif
